a9ccba0bd5c03cfa04ca9a1e346e8f30048a9366
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-12-12 21:36:02 +0530

    Update 2.cpp

--- Practile/PR. 1 The New Era/2.cpp
+++ Practile/PR. 1 The New Era/2.cpp
@@ -1,29 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class time{
-	
-	public:
-		
+class time
+{
+
+public:
 	int hour;
 	int minute;
 	int second;
-	int n;
-
-	
+	int number;
 };
 
-int main(){
-	time obj;
-	
-	
-	cout << "Enter second :";
-	cin >> obj.n;
-	
-	obj.hour = obj.n/3600;
-	obj.minute = (obj.n%3600)/60;
-	obj.second = obj.n%60;
-	
-	cout << "Time in HH:MM:SS format : " <<obj.hour << ": " <<obj.minute << ": " <<obj.second;
-}
+int main()
+{
+	time t1;
 
+	cout << "Enter second :";
+	cin >> t1.number;
+
+	t1.hour = t1.number / 3600;
+	t1.minute = (t1.number % 3600) / 60;
+	t1.second = t1.number % 60;
+
+	cout << "Time :" << t1.hour << " : " << t1.minute << " : " << t1.second;
+}
