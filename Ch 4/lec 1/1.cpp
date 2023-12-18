2da4afc01bd874721d9f14f4ce1e0baccf47cde0
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-12-18 17:54:52 +0530

    Add files via upload

---
+++ Ch 4/lec 1/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class FirstClass
+{
+public:
+    int a, b, c;
+};
+
+class SecondClass : public FirstClass;
+{
+
+};
+
+int main()
+{
+
+    return 0;
+}
