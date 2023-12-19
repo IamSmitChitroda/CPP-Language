a6ceb414896291a5ebaedd0bf0e55e63bcba4f73
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-12-19 10:40:48 +0530

    Add files via upload

---
+++ Practile/PR. 3 Heritage/1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+protected:
+    double width, height;
+
+public:
+    Shape()
+    {
+        cout << "Enter Width : ";
+        cin >> width;
+        cout << "Enter Height : ";
+        cin >> height;
+    }
+};
+
+class Triangle : public Shape
+{
+public:
+    void Area_of_Triangle()
+    {
+        cout << "Area of Triangle : " << 1 / 2 * width * height << endl;
+    }
+};
+
+class Rectangle : public Shape
+{
+public:
+    void Area_of_Rectangle()
+    {
+        cout << "Area of Triangle : " << width * height << endl;
+    }
+};
+
+int main()
+{
+    Triangle obj1;
+    Rectangle obj2;
+
+    obj1.Area_of_Triangle();
+    obj2.Area_of_Rectangle();
+    return 0;
+}
